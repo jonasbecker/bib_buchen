b6d51f738a1aa870c0dcc0bafcf7ed981b6c0da3
Jonas Becker <jonas.becker@codecentric.cloud>
2026-01-15 13:49:32 +0100

    test

---
+++ src/main/java/com/example/bib_buchen/BibBuchenApplication.java
@@ -0,0 +1,13 @@
+package com.example.bib_buchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibBuchenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibBuchenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bib_buchen/BibBuchenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bib_buchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibBuchenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
